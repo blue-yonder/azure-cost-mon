b1e30ac01fc594c4dabd444c1f5c714918197c3a
ManuelBahr <ManuelBahr@users.noreply.github.com>
2017-05-11 13:34:18 +0200

    add deploy to testpypi on every commit

--- setup.py
+++ setup.py
@@ -5,12 +5,21 @@
 from setuptools import setup
 
 setup(
-    name='azure-billing-exporter',
+    name='azure-cost-mon',
     description='Prometheus scraper for the Azure billing API',
     author='Manuel Bähr',
     author_email='manuel.baehr@blue-yonder.com',
+    url='https://github.com/blue-yonder/azure-cost-mon',
     use_scm_version=True,
     packages=['azure_billing'],
     setup_requires=['wheel', 'setuptools_scm'],
     install_requires=['flask', 'requests', 'numpy', 'pandas'],
+    classifiers=['Development Status :: 4 - Beta',
+                 'Intended Audience :: Developers',
+                 'Intended Audience :: System Administrators',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: POSIX :: Linux',
+                 'Operating System :: MacOS :: MacOS X',
+                 'Programming Language :: Python',
+                 'Topic :: System :: Monitoring']
 )
