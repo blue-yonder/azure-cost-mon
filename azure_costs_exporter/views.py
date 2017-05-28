ba61ef67fed172e788fe173961aa984f1d6b2f82
ManuelBahr <ManuelBahr@users.noreply.github.com>
2017-05-29 00:52:37 +0200

    refactored for proper usage of the collector class
    moved scrape code into the collector class.

--- azure_costs_exporter/views.py
+++ azure_costs_exporter/views.py
@@ -1,6 +1,5 @@
-from flask import Blueprint, current_app, Response, abort
-from prometheus_client import generate_latest, CollectorRegistry, CONTENT_TYPE_LATEST
-from .scrape import query_metrics
+from flask import Blueprint, Response, abort
+from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
 
 bp = Blueprint('views', __name__)
 
@@ -9,22 +8,10 @@ def health():
     return 'ok'
 
 
-@bp.route("/metrics", methods=['GET'])
+@bp.route("/metrics")
 def metrics():
-
-    registry = CollectorRegistry()
-
     try:
-        query_metrics(registry,
-                      current_app.config['ENROLLMENT_NUMBER'],
-                      current_app.config['BILLING_API_ACCESS_KEY'],
-                      current_app.config.get('PROMETHEUS_METRIC_NAME', 'azure_costs')
-                                )
+        content = generate_latest()
+        return content, 200, {'Content-Type': CONTENT_TYPE_LATEST}
     except Exception as e:
-        abort(Response("Scrape failed: {}".format(e),
-                       status=502)
-              )
-
-    content = generate_latest(registry)
-
-    return content, 200, {'Content-Type': CONTENT_TYPE_LATEST}
+        abort(Response("Scrape failed: {}".format(e), status=502))
--- tests/test_collector.py
+++ tests/test_collector.py
@@ -2,15 +2,15 @@ import pytest, responses
 import datetime
 from prometheus_client import generate_latest, CollectorRegistry
 
-from azure_costs_exporter.scrape import current_month, convert_json_df, extract_metrics_from_df
-from azure_costs_exporter.scrape import base_columns, cost_column, get_azure_data, query_metrics
+from azure_costs_exporter.prometheus_collector import convert_json_df, AzureEABillingCollector
+from azure_costs_exporter.prometheus_collector import base_columns, cost_column
 
 from .data import sample_data
 
 
-def test_month():
-    now = datetime.datetime.now()
-    assert current_month() == now.strftime("%Y-%m")
+def current_month():
+     now = datetime.datetime.now()
+     return now.strftime("%Y-%m")
 
 
 def test_df_conversion():
@@ -28,10 +28,23 @@ def test_df_missing_column():
                                    ExtendedCost=1.2)])
 
 
+@responses.activate
 def test_extract_metrics():
-    df = convert_json_df(sample_data)
+
+    enrollment = '123'
+    base_url = "https://ea.azure.com/rest/{}/usage-report".format(enrollment)
+    params = "?month={}&type=detail&fmt=Json".format(current_month())
+
+    responses.add(
+        method='GET',
+        url=base_url+params,
+        match_querystring=True,
+        json=sample_data
+    )
+
     registry = CollectorRegistry()
-    extract_metrics_from_df(df, 'costs', '123', registry)
+    c = AzureEABillingCollector('costs', enrollment, 'ab123xy')
+    registry.register(c)
 
     result = generate_latest(registry).decode('utf8').split('\n')
     assert len(result) == 5
@@ -47,6 +60,7 @@ def test_extract_metrics():
 def test_get_azure_data():
 
     enrollment='12345'
+    c = AzureEABillingCollector('cloud_costs', enrollment, 'abc123xyz')
 
     responses.add(
         method='GET',
@@ -55,26 +69,5 @@ def test_get_azure_data():
         json=sample_data
     )
 
-    data = get_azure_data(enrollment, 'abc123xyz', '2017-03')
+    data = c._get_azure_data('2017-03')
     assert data == sample_data
-
-
-@responses.activate
-def test_data():
-
-    enrollment='12345'
-    access_key='abc123xyz'
-    metric_name = 'cloud_costs'
-
-    responses.add(
-        method='GET',
-        url="https://ea.azure.com/rest/{}/usage-report?month=2017-03&type=detail&fmt=Json".format(enrollment),
-        match_querystring=True,
-        json=sample_data
-    )
-    registry = CollectorRegistry()
-    query_metrics(registry, enrollment, access_key, metric_name, month='2017-03')
-
-    prom_data = generate_latest(registry)
-
-    assert prom_data.decode('utf8').count(metric_name) == 4
