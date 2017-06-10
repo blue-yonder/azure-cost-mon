2fc1785bfcb9f9856c6755a7f18e092aa8971e04
Patrick Mühlbauer <pm.muehlbauer@gmail.com>
2017-06-10 15:11:43 +0200

    Use 'X-Prometheus-Scrape-Timeout-Seconds' header instead of hardcoded timeout. #7 (#11)
    * Use 'Scrape-Timeout-Seconds' header instead of hardcoded timeout. #7
    * Create collector instance in view function to use scrape timeout

--- azure_costs_exporter/main.py
+++ azure_costs_exporter/main.py
@@ -1,29 +1,13 @@
 from os import getcwd
 from flask import Flask
-from prometheus_client import REGISTRY
-from azure_costs_exporter.prometheus_collector import AzureEABillingCollector
 
 
 def create_app():
     from azure_costs_exporter.views import bp
-
     app = Flask(__name__)
     app.config.from_pyfile(getcwd()+"/application.cfg")
     app.register_blueprint(bp)
-
-    collector = AzureEABillingCollector(
-                    app.config['PROMETHEUS_METRIC_NAME'],
-                    app.config['ENROLLMENT_NUMBER'],
-                    app.config['BILLING_API_ACCESS_KEY']
-                )
-    try:
-        REGISTRY.register(collector)
-    except ValueError:
-        #don't register multiple times
-        pass
-
     return app
-
 
 
 if __name__ == "__main__":
--- azure_costs_exporter/views.py
+++ azure_costs_exporter/views.py
@@ -1,7 +1,19 @@
-from flask import Blueprint, Response, abort
-from prometheus_client import generate_latest, CONTENT_TYPE_LATEST
+from flask import Blueprint, Response, abort, current_app, request
+from prometheus_client import CONTENT_TYPE_LATEST, CollectorRegistry, generate_latest
+
+from .prometheus_collector import AzureEABillingCollector
+
 
 bp = Blueprint('views', __name__)
+DEFAULT_SCRAPE_TIMEOUT = 10
+
+
+def _get_timeout():
+    try:
+        return float(request.headers.get('X-Prometheus-Scrape-Timeout-Seconds'))
+    except Exception:
+        return DEFAULT_SCRAPE_TIMEOUT
+
 
 @bp.route("/health")
 def health():
@@ -10,8 +22,17 @@ def health():
 
 @bp.route("/metrics")
 def metrics():
+    timeout = _get_timeout()
+    collector = AzureEABillingCollector(
+        current_app.config['PROMETHEUS_METRIC_NAME'],
+        current_app.config['ENROLLMENT_NUMBER'],
+        current_app.config['BILLING_API_ACCESS_KEY'],
+        timeout
+    )
+    registry = CollectorRegistry()
+    registry.register(collector)
     try:
-        content = generate_latest()
+        content = generate_latest(registry)
         return content, 200, {'Content-Type': CONTENT_TYPE_LATEST}
     except Exception as e:
         abort(Response("Scrape failed: {}".format(e), status=502))
